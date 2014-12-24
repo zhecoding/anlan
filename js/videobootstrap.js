1c9a962f50a899e0761584e4a73b5f3f610139a2
lukeschlangen <lukeschlangen@gmail.com>
2014-12-23 19:42:28 -0600

    First Commit
    Rough Draft

---
+++ js/videobootstrap.js
@@ -0,0 +1,97 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+//youtube player functionality
+$('.youtube-trigger').click(function() {
+    var src = this.value;
+    $('#youtubeVideoModal').modal('show');
+    $('#youtubeVideoModal iframe').attr('src', src);
+
+    resizeYoutubeVideo();
+});
+
+$('#youtubeVideoModal').on('hidden.bs.modal', function() {
+    $('#youtubeVideoModal iframe').removeAttr('src');
+});
+
+$('#youtubeVideoModal').on('shown.bs.modal', function () {
+    resizeYoutubeVideo();
+  })
+
+$(window).resize(function() {
+        resizeYoutubeVideo();
+});
+
+function resizeYoutubeVideo() {
+    $('#youtubeIframe').width('100%');
+    var newHeight = $('.modal-lg').width() * 9 / 16;
+    $('#youtubeIframe').height(newHeight);
+}
+
+function printDiv(divName) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+
+     document.body.innerHTML = printContents;
+
+     window.print();
+
+    //  document.body.innerHTML = originalContents;
+     location.reload();
+}
+
+$(document).ready(function () {
+
+    $(".player").mb_YTPlayer();
+
+});
+
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
