20a97fcfbf25af3eb2aab559912c21084316975f
Luke Schlangen <lukeschlangen@gmail.com>
2014-12-30 13:56:51 -0600

    Deleting Duplicate Sections
    Scrolling section was duplicated and has been removed

--- js/videobootstrap.js
+++ js/videobootstrap.js
@@ -1,14 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
@@ -73,10 +62,10 @@ $(document).ready(function () {
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
+    if ($(".navbar").offset().top < 50) {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");  
     }
 });
 
